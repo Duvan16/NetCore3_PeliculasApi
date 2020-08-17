a72d445a57ad5e60ef861e933543cc2d81e5dd92
Duvan <duvan4448@gmail.com>
2020-08-17 11:44:00 -0500

    1-ConfigurarEntityFramework

---
+++ NetCore3_PeliculasApi/NetCore3_PeliculasApi/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore3_PeliculasApi.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore3_PeliculasApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }       
+    }
+}
