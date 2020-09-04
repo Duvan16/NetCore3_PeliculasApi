86b563e4c96b14a0a785aa8ae7bfcb740e0cbe45
Duvan <duvan4448@gmail.com>
2020-09-03 23:17:46 -0500

    33-PruebasUnitarias_Generos

---
+++ NetCore3_PeliculasApi/NetCore3_PeliculasApi.Tests/BasePruebas.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NetCore3_PeliculasApi.Helpers;
+using NetTopologySuite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore3_PeliculasApi.Tests { 
+    public class BasePruebas
+    {
+        protected ApplicationDbContext ConstruirContext(string nombreDB)
+        {
+            var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(nombreDB).Options;
+
+            var dbContext = new ApplicationDbContext(opciones);
+            return dbContext;
+        }
+
+        protected IMapper ConfigurarAutoMapper()
+        {
+            var config = new MapperConfiguration(options =>
+           {
+               var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+               options.AddProfile(new AutoMapperProfiles(geometryFactory));
+           });
+
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ NetCore3_PeliculasApi/NetCore3_PeliculasApi.Tests/PruebasUnitarias/GenerosControllerTests.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetCore3_PeliculasApi.Controllers;
+using NetCore3_PeliculasApi.DTOs;
+using NetCore3_PeliculasApi.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore3_PeliculasApi.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class GenerosControllerTests : BasePruebas
+    {
+        [TestMethod]
+        public async Task ObtenerTodosLosGeneros()
+        {
+            // Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+
+            contexto.Generos.Add(new Genero() { Nombre = "Género 1" });
+            contexto.Generos.Add(new Genero() { Nombre = "Género 2" });
+            await contexto.SaveChangesAsync();
+
+            var contexto2 = ConstruirContext(nombreBD);
+
+            // Prueba
+            var controller = new GenerosController(contexto2, mapper);
+            var respuesta = await controller.Get();
+
+            // Verificación
+            var generos = respuesta.Value;
+            Assert.AreEqual(2, generos.Count);
+        }
+
+        [TestMethod]
+        public async Task ObtenerGeneroPorIdNoExistente()
+        {
+            // Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+
+            //Prueba
+            var controller = new GenerosController(contexto, mapper);
+            var respuesta = await controller.Get(1);
+
+            //Verificación
+            var resultado = respuesta.Result as StatusCodeResult;
+            Assert.AreEqual(404, resultado.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task ObtenerGeneroPorIdExistente()
+        {
+            // Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+
+            contexto.Generos.Add(new Genero() { Nombre = "Género 1" });
+            contexto.Generos.Add(new Genero() { Nombre = "Género 2" });
+            await contexto.SaveChangesAsync();
+
+            var contexto2 = ConstruirContext(nombreBD);
+
+            // Prueba
+            var controller = new GenerosController(contexto2, mapper);
+            var id = 1;
+            var respuesta = await controller.Get(id);
+
+            // Verificación
+            var resultado = respuesta.Value;
+            Assert.AreEqual(id, resultado.Id);
+        }
+
+        [TestMethod]
+        public async Task CrearGenero()
+        {
+            // Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+
+            //Prueba
+            var nuevoGenero = new GeneroCreacionDTO() { Nombre = "nuevo género" };
+            var controller = new GenerosController(contexto, mapper);
+            var respuesta = await controller.Post(nuevoGenero);
+
+            //Verificación
+            var resultado = respuesta as CreatedAtRouteResult;
+            Assert.IsNotNull(resultado);
+
+            var contexto2 = ConstruirContext(nombreBD);
+            var cantidad = await contexto2.Generos.CountAsync();
+            Assert.AreEqual(1, cantidad);
+        }
+
+        [TestMethod]
+        public async Task ActualizarGenero()
+        {
+            // Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+
+            contexto.Generos.Add(new Genero() { Nombre = "Género 1" });
+            await contexto.SaveChangesAsync();
+
+            var contexto2 = ConstruirContext(nombreBD);
+            var controller = new GenerosController(contexto2, mapper);
+
+            //Pruebas
+            var generoCreacionDTO = new GeneroCreacionDTO() { Nombre = "Nuevo nombre" };
+
+            var id = 1;
+            var respuesta = await controller.Put(id, generoCreacionDTO);
+
+            //Verificación
+            var resultado = respuesta as StatusCodeResult;
+            Assert.AreEqual(204, resultado.StatusCode);
+
+            var contexto3 = ConstruirContext(nombreBD);
+            var existe = await contexto3.Generos.AnyAsync(x => x.Nombre == "Nuevo nombre");
+            Assert.IsTrue(existe);
+        }
+
+        [TestMethod]
+        public async Task IntentarBorrarGeneroNoExistente()
+        {
+            //Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+
+            //Prueba
+            var controller = new GenerosController(contexto, mapper);
+            var respuesta = await controller.Delete(1);
+            
+            //Verificación
+            var resultado = respuesta as StatusCodeResult;
+            Assert.AreEqual(404, resultado.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task BorrarGenero()
+        {
+            //Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+            contexto.Generos.Add(new Genero() { Nombre = "Género 1" });
+            await contexto.SaveChangesAsync();
+
+            //Pruebas
+            var contexto2 = ConstruirContext(nombreBD);
+            var controller = new GenerosController(contexto2, mapper);
+            var respuesta = await controller.Delete(1);
+
+            //Verificación
+            var resultado = respuesta as StatusCodeResult;
+            Assert.AreEqual(204, resultado.StatusCode);
+
+            var contexto3 = ConstruirContext(nombreBD);
+            var existe = await contexto3.Generos.AnyAsync();
+            Assert.IsFalse(existe);
+
+        }
+    }
+}
