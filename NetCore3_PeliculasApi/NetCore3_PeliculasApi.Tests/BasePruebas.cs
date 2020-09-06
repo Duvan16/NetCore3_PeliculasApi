b80264225d862ef3ae771a594200f0a704ce584d
Duvan <duvan4448@gmail.com>
2020-09-06 17:20:02 -0500

    38-PruebasUnitarias-Reviews

--- NetCore3_PeliculasApi/NetCore3_PeliculasApi.Tests/BasePruebas.cs
+++ NetCore3_PeliculasApi/NetCore3_PeliculasApi.Tests/BasePruebas.cs
@@ -1,14 +1,20 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NetCore3_PeliculasApi.Helpers;
 using NetTopologySuite;
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
 
 namespace NetCore3_PeliculasApi.Tests { 
     public class BasePruebas
     {
+        protected string usuarioPorDefectoId = "adadasdad-aasda-asda3-2312as1-123213213";
+        protected string usuarioPorDefectoEmail = "miejemplo@hotmail.com";
+
         protected ApplicationDbContext ConstruirContext(string nombreDB)
         {
             var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -27,6 +33,21 @@ namespace NetCore3_PeliculasApi.Tests {
            });
 
             return config.CreateMapper();
+        }
+
+        protected ControllerContext ConstruirControllerContext()
+        {
+            var usuario = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name,usuarioPorDefectoEmail),
+                new Claim(ClaimTypes.Email,usuarioPorDefectoEmail),
+                new Claim(ClaimTypes.NameIdentifier,usuarioPorDefectoId)
+            }));
+
+            return new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = usuario }
+            };
         }
     }
 }
